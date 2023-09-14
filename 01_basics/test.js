adbf8947c631b19c8e5dd31e9cb0cd1d0fd135c1
Sk Moinuddin <126265804+Moinuddin2@users.noreply.github.com>
2023-09-14 18:51:05 +0000

    DataType

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
-console.log("hello")
+// console.log("hello")
+let name ="Rubel";
+var age =22;
+city ="kolkata" ;
+let village;
+console.table([name,age,city]);
+console.log(typeof "rubel");
+console.log(village);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof(age));
