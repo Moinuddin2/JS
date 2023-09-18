3eb0b9b52555e9ad0ca336a0272941e98e60cb01
Sk Moinuddin <126265804+Moinuddin2@users.noreply.github.com>
2023-09-18 14:58:41 +0000

    DataType Summary

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+const score = 100;
+const scoreValue = 100.7; // Javascript is not matter for floator decimal number
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail; 
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id == anotherId);
+// const bigNumber = 322454654789561253n;
+//  Array Declare
+const heros = ["Hello", "My", "Dear"];
+// Object Declare
+let myObj = {
+    name:"Rubel",
+    age: 22,
+}
+// Fuinction declare
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof(anotherId));
