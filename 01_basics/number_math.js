ed148ab2567d138fd032ea8c6f059ec1a247ef26
Sk Moinuddin <126265804+Moinuddin2@users.noreply.github.com>
2023-09-19 19:50:14 +0000

    Number and Math summary

---
+++ 01_basics/number_math.js
@@ -0,0 +1,13 @@
+const balance =100;
+console.log(balance);
+const score = new Number(100)
+console.log(score);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+// Math
+console.log(Math);
+console.log(Math.abs(-4));//absolute
+console.log(Math.round(8.7));//round
+console.log(Math.ceil(6.7));
+console.log(Math.floor(6.9));
