af03a38a1fb9692a65d31c5179dc96874bf33012
Sk Moinuddin <126265804+Moinuddin2@users.noreply.github.com>
2023-09-20 05:20:45 +0000

    Number and Math Summary

--- 01_basics/number_math.js
+++ 01_basics/number_math.js
@@ -1,13 +1,22 @@
-const balance =100;
-console.log(balance);
-const score = new Number(100)
-console.log(score);
+// const balance =100;
+// console.log(balance);
+// const score = new Number(100)
+// console.log(score);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
-// Math
-console.log(Math);
-console.log(Math.abs(-4));//absolute
-console.log(Math.round(8.7));//round
-console.log(Math.ceil(6.7));
-console.log(Math.floor(6.9));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// // // Math
+// console.log(Math);
+// console.log(Math.abs(-4));//absolute
+// console.log(Math.round(8.7));//round
+// console.log(Math.ceil(6.7));
+// console.log(Math.floor(6.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(5,6,7,9));
+console.log(Math.random());
+console.log((Math.random() *10) +1)
+console.log((Math.random() *10) +1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min +1))  + min);
