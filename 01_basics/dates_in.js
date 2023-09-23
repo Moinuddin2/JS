96b45d68d3be0acbceede237208da1cb603db68f
Sk Moinuddin <126265804+Moinuddin2@users.noreply.github.com>
2023-09-23 15:21:55 +0000

    Date_Time_Summary

--- 01_basics/dates_in.js
+++ 01_basics/dates_in.js
@@ -1,11 +1,30 @@
 // Dates
-let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23)
 // console.log(myCreatedDate.toDateString()); 
-let myCreatedDate = new Date(2023, 8, 20, 11, 43);
-console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate = new Date(2023, 9, 20, 11, 43);
+// // console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// // console.log(myTimeStamp);
+// // console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+ 
+newDate.toLocaleDateString('default', {
+    weekday: "long"
+})
+
+
+
--- 01_basics/number_math.js
+++ 01_basics/number_math.js
@@ -1,19 +1,19 @@
-// const balance =100;
-// console.log(balance);
-// const score = new Number(100)
-// console.log(score);
+const balance =100;
+console.log(balance);
+const score = new Number(100)
+console.log(score);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
-// // // Math
-// console.log(Math);
-// console.log(Math.abs(-4));//absolute
-// console.log(Math.round(8.7));//round
-// console.log(Math.ceil(6.7));
-// console.log(Math.floor(6.9));
-// console.log(Math.min(4,3,6,8));
-// console.log(Math.max(5,6,7,9));
-console.log(Math.random());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+// // // // Math
+// // console.log(Math);
+// // console.log(Math.abs(-4));//absolute
+// // console.log(Math.round(8.7));//round
+console.log(Math.ceil(6.7));
+console.log(Math.floor(6.9));
+// // console.log(Math.min(4,3,6,8));
+// // console.log(Math.max(5,6,7,9));
+// console.log(Math.random());
 console.log((Math.random() *10) +1)
 console.log((Math.random() *10) +1)
 
